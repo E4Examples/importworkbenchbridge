a9d276ad0cba7e6afee7179bbd9cc7a37bf769ed
Wim Jongman <wim.jongman@remainsoftware.com>
2014-01-23 12:06:18 +0100

    More refinements

--- com.remainsoftware.e4.model.importer/src/com/remainsoftware/e4/model/importer/ModelRegistration.java
+++ com.remainsoftware.e4.model.importer/src/com/remainsoftware/e4/model/importer/ModelRegistration.java
@@ -5,17 +5,17 @@ import java.util.concurrent.Executors;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
-import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.ui.IStartup;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceReference;
+import org.eclipse.ui.PlatformUI;
 
 public class ModelRegistration implements IStartup {
 	ExecutorService threadPool = Executors.newFixedThreadPool(10);
-	
+
 	@Override
 	public void earlyStartup() {
 		threadPool.execute(new Runnable() {
@@ -25,33 +25,28 @@ public class ModelRegistration implements IStartup {
 
 				IExtensionRegistry registry = Activator.getService(IExtensionRegistry.class);
 
-				ServiceReference<?>[] allServiceReferences;
-				try {
-					allServiceReferences = Activator.getContext().getAllServiceReferences(null, null);
-					for (ServiceReference<?> serviceReference : allServiceReferences) {
-
-						String[] classes = ((String[]) serviceReference.getProperty("objectClass"));
-						for (String string : classes) {
-							if (string.contains("e4"))
-								System.out.println(string);
-						}
-					}
-				} catch (InvalidSyntaxException e) {
-				}
-
-				IConfigurationElement[] elements = registry.getConfigurationElementsFor(Activator.getContext()
-						.getBundle().getSymbolicName() + ".modelimport");
+				IConfigurationElement[] elements = registry.getConfigurationElementsFor(Activator
+						.getContext().getBundle().getSymbolicName()
+						+ ".modelimport");
 				for (IConfigurationElement element : elements) {
 					String uri = element.getAttribute("modelURI");
 					String id = element.getAttribute("modelId");
-					String reParent = element.getAttribute("reparentId");
+					String reParent = element.getAttribute("targetId");
+					String relationship = element.getAttribute("relationship");
 					MUIElement muiElement = ApplicationModelUtil.loadModel(uri, id, registry);
-					IWorkbench model = Activator.getService(IWorkbench.class);
-					EModelService modelService = model.getApplication().getContext()
-							.get(EModelService.class);
-					MUIElement parentElement = modelService.find(reParent, model.getApplication());
+					IEclipseContext serviceContext = (IEclipseContext) PlatformUI.getWorkbench()
+							.getService(IEclipseContext.class);
+					EModelService modelService = serviceContext.get(EModelService.class);
+					MApplication application = serviceContext.get(MApplication.class);
+					MUIElement parentElement = modelService.find(reParent, application);
+					if (relationship.equals("sibling"))
+						parentElement = parentElement.getParent();
 
-					// this is just a hack (try adding a part to a partstack)
+					/*
+					 * The addition below is just a quick hack and needs more
+					 * work. For now it is enough to, for example, add a part to
+					 * a partstack
+					 */
 					if (parentElement instanceof MElementContainer) {
 						MElementContainer cont = (MElementContainer) parentElement;
 						cont.getChildren().add(muiElement);
