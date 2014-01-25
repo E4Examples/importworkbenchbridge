85c0c3f4f850f62920e8517c558ef07547342a38
Wim Jongman <wim.jongman@remainsoftware.com>
2014-01-25 18:56:08 +0100

    Refactoring

--- com.remainsoftware.e4.model.importer/src/com/remainsoftware/e4/model/importer/ApplicationModelUtil.java
+++ com.remainsoftware.e4.model.importer/src/com/remainsoftware/e4/model/importer/ApplicationModelUtil.java
@@ -8,6 +8,7 @@ import org.eclipse.e4.ui.internal.workbench.E4XMIResourceFactory;
 import org.eclipse.e4.ui.internal.workbench.ModelServiceImpl;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.impl.ApplicationPackageImpl;
+import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.services.internal.events.EventBroker;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
@@ -15,26 +16,44 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.ui.PlatformUI;
 
 public class ApplicationModelUtil {
 
 	private static ResourceSet resourceSet = new ResourceSetImpl();
 
-	public static MUIElement loadModel(String platformURI, String findId,
-			IExtensionRegistry registry) {
+	public static MApplication loadModel(String platformURI) {
 		ApplicationPackageImpl.init();
-
 		URI uri = URI.createURI(platformURI);
-
 		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
 				.put("e4xmi", new E4XMIResourceFactory());
-
 		Resource res = resourceSet.getResource(uri, true);
-		MApplication app = (MApplication) res.getContents().get(0);
+		return (MApplication) res.getContents().get(0);
+	}
+
+	public static void mergeModel(String uri, String id, String reParent, String relationship) {
+
+		MApplication app = loadModel(uri);
 		IEclipseContext context = EclipseContextFactory.create();
 		context.modify(IEventBroker.class, new EventBroker());
-		context.modify(IExtensionRegistry.class, registry);
+		context.modify(IExtensionRegistry.class, Activator.getService(IExtensionRegistry.class));
 		EModelService service = new ModelServiceImpl(context);
-		return service.find(findId, app);
+		MUIElement muiElement = service.find(reParent, app);
+		IEclipseContext serviceContext = (IEclipseContext) PlatformUI.getWorkbench().getService(
+				IEclipseContext.class);
+		EModelService modelService = serviceContext.get(EModelService.class);
+		MApplication application = serviceContext.get(MApplication.class);
+		MUIElement parentElement = modelService.find(reParent, application);
+		if (relationship.equals("sibling"))
+			parentElement = parentElement.getParent();
+
+		/*
+		 * The addition below is just a quick hack and needs more work. For now
+		 * it is enough to, for example, add a part to a partstack
+		 */
+		if (parentElement instanceof MElementContainer) {
+			MElementContainer cont = (MElementContainer) parentElement;
+			cont.getChildren().add(muiElement);
+		}
 	}
 }
--- com.remainsoftware.e4.model.importer/src/com/remainsoftware/e4/model/importer/ModelRegistration.java
+++ com.remainsoftware.e4.model.importer/src/com/remainsoftware/e4/model/importer/ModelRegistration.java
@@ -1,59 +1,23 @@
 package com.remainsoftware.e4.model.importer;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
-import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.ui.MElementContainer;
-import org.eclipse.e4.ui.model.application.ui.MUIElement;
-import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.ui.IStartup;
-import org.eclipse.ui.PlatformUI;
 
 public class ModelRegistration implements IStartup {
-	ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
 	@Override
 	public void earlyStartup() {
-		threadPool.execute(new Runnable() {
-
-			@Override
-			public void run() {
-
-				IExtensionRegistry registry = Activator.getService(IExtensionRegistry.class);
-
-				IConfigurationElement[] elements = registry.getConfigurationElementsFor(Activator
-						.getContext().getBundle().getSymbolicName()
-						+ ".modelimport");
-				for (IConfigurationElement element : elements) {
-					String uri = element.getAttribute("modelURI");
-					String id = element.getAttribute("modelId");
-					String reParent = element.getAttribute("targetId");
-					String relationship = element.getAttribute("relationship");
-					MUIElement muiElement = ApplicationModelUtil.loadModel(uri, id, registry);
-					IEclipseContext serviceContext = (IEclipseContext) PlatformUI.getWorkbench()
-							.getService(IEclipseContext.class);
-					EModelService modelService = serviceContext.get(EModelService.class);
-					MApplication application = serviceContext.get(MApplication.class);
-					MUIElement parentElement = modelService.find(reParent, application);
-					if (relationship.equals("sibling"))
-						parentElement = parentElement.getParent();
-
-					/*
-					 * The addition below is just a quick hack and needs more
-					 * work. For now it is enough to, for example, add a part to
-					 * a partstack
-					 */
-					if (parentElement instanceof MElementContainer) {
-						MElementContainer cont = (MElementContainer) parentElement;
-						cont.getChildren().add(muiElement);
-					}
-				}
-			}
-		});
+		IExtensionRegistry registry = Activator.getService(IExtensionRegistry.class);
+		IConfigurationElement[] elements = registry.getConfigurationElementsFor(Activator
+				.getContext().getBundle().getSymbolicName()
+				+ ".modelimport");
+		for (IConfigurationElement element : elements) {
+			String uri = element.getAttribute("modelURI");
+			String id = element.getAttribute("modelId");
+			String reParent = element.getAttribute("targetId");
+			String relationship = element.getAttribute("relationship");
+			ApplicationModelUtil.mergeModel(uri, id, reParent, relationship);
+		}
 	}
-
 }
